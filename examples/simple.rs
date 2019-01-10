ff2e18d117cd3235e3afd30dc52ee3b52f4bd384
Jacob Brown <kardeiz@gmail.com>
2019-01-10 15:46:43 -0600

    list_records mostly complete

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,30 @@
+#![recursion_limit="128"]
+
 pub fn main() -> oai::err::Result<()> {
     use futures::{Future, IntoFuture};
 
-    let client = oai::Client::new("https://demo.dspace.org/oai/")?;
+    // let client = oai::Client::new("https://demo.dspace.org/oai/")?;
 
-    let fut = client
-        .list_records::<oai::pref::OaiDc>()
-        .map_err(|e| println!("{:?}", e))
-        .map(|pkg| println!("{:?}", pkg));
+    
+
+    let fut = {
+        let client = std::sync::Arc::new(oai::Client::new("https://repository.tcu.edu/oai/")?); 
+        let params = oai::list_records::Params {
+            set: Some("col_116099117_11012".into()),
+            ..oai::list_records::Params::default()
+        };
+
+        client
+            .list_records_all::<oai::pref::Xoai>(Some(params))
+            .map_err(|e| { println!("{:?}", e); () } )
+            .map(|t| { println!("{:?}", t.records.len()); () } )
+            // .map(|t| { println!("{}", &serde_json::to_string_pretty(&t).unwrap()); () } )           
+            // .and_then(move |t| {
+            //     t.get_next(client_2.as_ref())
+            //         .map(|t| { println!("{:?}", t); () } )
+            //         .map_err(|e| { println!("{:?}", e); () } )
+            // })
+    };
 
     let _ = tokio::run(fut);
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ pub mod err {
 
 use futures::{future::Either as EitherFuture, Future, IntoFuture, Stream};
 use serde_derive::{Deserialize, Serialize};
+use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct Client {
-    inner: reqwest::r#async::Client,
-    base_url: url::Url
+    pub (crate) inner: reqwest::r#async::Client,
+    pub (crate) base_url: url::Url
 }
 
 impl Client {
@@ -22,25 +24,28 @@ pub mod pref {
 
     use serde_json::json;
 
+    use serde_derive::{Deserialize, Serialize};
+
+    use crate::err;
+    use std::collections::HashMap;
+
     pub trait MetadataPrefix: std::fmt::Debug {
         type MetadataType: std::fmt::Debug;
 
         fn as_str() -> &'static str;
-        fn parse_metadata(node: &roxmltree::Node) -> crate::err::Result<Self::MetadataType>;
+        fn parse_metadata(node: &roxmltree::Node) -> err::Result<Self::MetadataType>;
     }
 
     #[derive(Debug)]
     pub struct OaiDc;
-    #[derive(Debug)]
-    pub struct Xoai;
 
     impl MetadataPrefix for OaiDc {
-        type MetadataType = serde_json::Value;
+        type MetadataType = HashMap<String, Vec<Option<String>>>;
 
         fn as_str() -> &'static str { "oai_dc" }
 
-        fn parse_metadata(node: &roxmltree::Node) -> crate::err::Result<Self::MetadataType> {
-            let mut map = std::collections::HashMap::new();
+        fn parse_metadata(node: &roxmltree::Node) -> err::Result<Self::MetadataType> {
+            let mut map = HashMap::new();
 
             for dc_node in node
                 .children()
@@ -50,20 +55,65 @@ pub mod pref {
             {
                 map.entry(String::from(dc_node.tag_name().name()))
                     .or_insert_with(Vec::new)
-                    .push(json!(dc_node.text()));
+                    .push(dc_node.text().map(String::from));
             }
 
-            Ok(json!(map))
+            Ok(map)
         }
     }
 
+    #[derive(Debug)]
+    pub struct Xoai;
+
+    #[derive(Debug, Serialize)]
+    pub struct XoaiElements(pub Vec<XoaiElement>);
+
+    #[derive(Debug, Serialize)]
+    pub struct XoaiElement {
+        name: String,
+        fields: Option<Vec<(Option<String>, Option<String>)>>,
+        children: Option<Vec<Box<XoaiElement>>>
+    }
+
+    impl XoaiElement {
+
+        fn from_node(node: &roxmltree::Node) -> err::Result<Self> {
+            let name = node.attribute("name").ok_or_else(|| err::msg("No name"))?.to_string();
+            let fields = node.children()
+                .filter(|x| x.has_tag_name("field"))
+                .map(|x| {
+                    (x.attribute("name").map(String::from), x.text().map(String::from))
+                })
+                .collect::<Vec<_>>();
+            let children = node.children()
+                .filter(|x| x.has_tag_name("element") )
+                .map(|x| XoaiElement::from_node(&x).map(Box::new) )
+                .collect::<err::Result<Vec<_>>>()?;
+            Ok(XoaiElement {
+                name,
+                fields: if fields.is_empty() { None } else { Some(fields) },
+                children: if children.is_empty() { None } else { Some(children) }                
+            })
+        }
+
+    }
+
     impl MetadataPrefix for Xoai {
-        type MetadataType = serde_json::Value;
+        type MetadataType = XoaiElements;
 
         fn as_str() -> &'static str { "xoai" }
 
-        fn parse_metadata(_node: &roxmltree::Node) -> crate::err::Result<Self::MetadataType> {
-            unimplemented!()
+        fn parse_metadata(node: &roxmltree::Node) -> err::Result<Self::MetadataType> {
+
+            let vec = node
+                .children()
+                .filter(|x| x.has_tag_name("metadata"))
+                .flat_map(|x| x.children())
+                .filter(|x| x.has_tag_name("element"))
+                .map(|x| XoaiElement::from_node(&x) )
+                .collect::<err::Result<Vec<_>>>()?;
+
+            Ok(XoaiElements(vec))
         }
     }
 
@@ -72,15 +122,16 @@ pub mod pref {
 pub mod record {
 
     use chrono::prelude::*;
+    use serde_derive::{Deserialize, Serialize};
 
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Clone, Serialize)]
     pub struct Header {
         pub identifier: String,
         pub datestamp: DateTime<Utc>,
         pub set_spec: Vec<String>
     }
 
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Clone, Serialize)]
     pub struct Record<T> {
         pub header: Header,
         pub metadata: T
@@ -94,36 +145,59 @@ pub mod list_records {
 
     use chrono::prelude::*;
     use std::marker::PhantomData;
+    use std::sync::Arc;
 
     use crate::{err, ext::NodeExt, pref, record, Client};
 
-    impl<T: pref::MetadataPrefix> ListRecords<T> {
-        fn build(text: String) -> err::Result<ListRecords<T>> {
-            let doc = roxmltree::Document::parse(&text)?;
+    #[derive(Debug, Serialize)]
+    pub struct ResumptionToken {
+        value: Option<String>,
+        complete_list_size: u64,
+        cursor: u64
+    }
 
-            // for child in doc.root().children() {
-            //     println!("{:?}", child.tag_name());
-            // }
+    #[derive(Debug, Serialize)]
+    pub struct ListRecords<T, U> {
+        pub response_date: DateTime<Utc>,
+        pub records: Vec<record::Record<U>>,
+        pub resumption_token: Option<ResumptionToken>,
+        #[serde(skip_serializing)]
+        marker: PhantomData<T>
+    }
+
+    #[derive(Default, Debug, Serialize)]
+    pub struct Params {
+        pub from: Option<DateTime<Utc>>,
+        pub until: Option<DateTime<Utc>>,
+        pub set: Option<String>
+    }
+
+    impl<T: pref::MetadataPrefix> ListRecords<T, T::MetadataType> {
+
+        pub fn has_next(&self) -> bool {
+            self.resumption_token.as_ref().and_then(|x| x.value.as_ref()).is_some()
+        }
+
+        pub fn get_next(self, client: &Client) -> impl Future<Item = ListRecords<T, T::MetadataType>, Error = err::Error> {
+            if let Some(rt) = self.resumption_token.as_ref()
+                .and_then(|x| x.value.as_ref())
+                .map(|x| String::from(x as &str)) {
+                EitherFuture::A(client.list_records_from_resumption_token(rt))
+            } else {
+                EitherFuture::B(futures::future::err(err::msg("No more results")))
+            }
+        }
+
+        fn build(text: String) -> err::Result<ListRecords<T, T::MetadataType>> {
+            let doc = roxmltree::Document::parse(&text)?;
 
             let root = doc.root_element();
 
-            let response_date = root
-                .children()
-                .filter(|x| x.has_tag_name("responseDate"))
-                .next()
-                .and_then(|x| x.text().and_then(|x| x.parse().ok()))
-                .ok_or_else(|| err::msg("No response date"))?;
+            let response_date = root.find_first_child_parsed_text("responseDate")?;
 
-            let list_records = root
-                .children()
-                .filter(|x| x.has_tag_name("ListRecords"))
-                .next()
-                .ok_or_else(|| err::msg("No ListRecords node"))?;
+            let list_records = root.find_first_child("ListRecords")?;
 
-            let resumption_token = list_records
-                .children()
-                .filter(|x| x.has_tag_name("resumptionToken"))
-                .next()
+            let resumption_token = list_records.find_first_child("resumptionToken").ok()
                 .and_then(|x| {
                     match (
                         x.text(),
@@ -143,19 +217,11 @@ pub mod list_records {
                 .children()
                 .filter(|x| x.has_tag_name("record"))
                 .flat_map(|x| -> err::Result<record::Record<T::MetadataType>> {
-                    let header = x.first("header")?;
+                    let header = x.find_first_child("header")?;
 
                     let header = record::Header {
-                        identifier: header
-                            .first("identifier")?
-                            .text()
-                            .ok_or_else(|| err::msg("No identifier"))?
-                            .into(),
-                        datestamp: header
-                            .first("datestamp")?
-                            .text()
-                            .ok_or_else(|| err::msg("No datestamp"))?
-                            .parse()?,
+                        identifier: header.find_first_child_parsed_text("identifier")?,
+                        datestamp: header.find_first_child_parsed_text("datestamp")?,
                         set_spec: header
                             .children()
                             .filter(|y| y.has_tag_name("setSpec"))
@@ -163,7 +229,7 @@ pub mod list_records {
                             .collect()
                     };
 
-                    let metadata_node = x.first("metadata")?;
+                    let metadata_node = x.find_first_child("metadata")?;
 
                     let metadata = T::parse_metadata(&metadata_node)?;
 
@@ -175,27 +241,19 @@ pub mod list_records {
         }
     }
 
-    #[derive(Debug)]
-    pub struct ResumptionToken {
-        value: Option<String>,
-        complete_list_size: u64,
-        cursor: u64
-    }
-
-    #[derive(Debug)]
-    pub struct ListRecords<T: pref::MetadataPrefix> {
-        response_date: DateTime<Utc>,
-        records: Vec<record::Record<T::MetadataType>>,
-        resumption_token: Option<ResumptionToken>,
-        marker: PhantomData<T>
-    }
+    
 
     impl Client {
         pub fn list_records<T: pref::MetadataPrefix>(
-            &self
-        ) -> impl Future<Item = ListRecords<T>, Error = err::Error> {
+            &self, params: Option<Params>
+        ) -> impl Future<Item = ListRecords<T, T::MetadataType>, Error = err::Error> {
+            
+            let params = params.unwrap_or_else(Params::default);
+
             #[derive(Serialize)]
             struct VerbedParams {
+                #[serde(flatten)]
+                params: Params,
                 #[serde(rename = "metadataPrefix")]
                 metadata_prefix: &'static str,
                 verb: &'static str
@@ -209,7 +267,7 @@ pub mod list_records {
                 return EitherFuture::B(futures::future::err(err::msg("Cannot set path")));
             }
 
-            let params = VerbedParams { metadata_prefix: T::as_str(), verb: "ListRecords" };
+            let params = VerbedParams { params, metadata_prefix: T::as_str(), verb: "ListRecords" };
 
             if let Ok(query) = serde_urlencoded::to_string(&params) {
                 url.set_query(Some(&query));
@@ -228,10 +286,87 @@ pub mod list_records {
                 })
                 .from_err()
                 .and_then(|body| String::from_utf8(body.to_vec()).into_future().from_err())
-                .and_then(|text| ListRecords::<T>::build(text).into_future().from_err());
+                .and_then(|text| ListRecords::<T, T::MetadataType>::build(text).into_future().from_err());
 
             EitherFuture::A(rt)
         }
+
+        pub fn list_records_from_resumption_token<T: pref::MetadataPrefix>(
+            &self, resumption_token: String
+        ) -> impl Future<Item = ListRecords<T, T::MetadataType>, Error = err::Error> {
+            
+            #[derive(Serialize)]
+            struct VerbedParams {
+                #[serde(rename="resumptionToken")]
+                resumption_token: String,
+                verb: &'static str
+            }
+
+            let mut url = self.base_url.clone();
+
+            if let Ok(mut path_segments) = url.path_segments_mut() {
+                path_segments.pop_if_empty().push("request");
+            } else {
+                return EitherFuture::B(futures::future::err(err::msg("Cannot set path")));
+            }
+
+            let params = VerbedParams { resumption_token, verb: "ListRecords" };
+
+            if let Ok(query) = serde_urlencoded::to_string(&params) {
+                url.set_query(Some(&query));
+            } else {
+                return EitherFuture::B(futures::future::err(err::msg("Cannot set query")));
+            }
+
+            let rt = self
+                .inner
+                .get(url)
+                .send()
+                .and_then(|mut res| {
+                    let body =
+                        std::mem::replace(res.body_mut(), reqwest::r#async::Decoder::empty());
+                    body.concat2()
+                })
+                .from_err()
+                .and_then(|body| String::from_utf8(body.to_vec()).into_future().from_err())
+                .and_then(|text| ListRecords::<T, T::MetadataType>::build(text).into_future().from_err());
+
+            EitherFuture::A(rt)
+        }
+
+        pub fn list_records_all<T: pref::MetadataPrefix>(
+            &self, params: Option<Params>
+        ) -> impl Future<Item = ListRecords<T, T::MetadataType>, Error = err::Error> {
+
+            fn run<T: pref::MetadataPrefix>(client: Client, params: Option<Params>)
+                -> impl Future<Item = ListRecords<T, T::MetadataType>, Error = err::Error> {
+
+                use futures::future::{loop_fn, Loop};
+
+                loop_fn(EitherFuture::A(client.list_records(params)), move |list_records_fut| {
+                    let client = client.clone();
+                    list_records_fut.and_then(move |mut list_records| {
+                        if list_records.has_next() {
+                            let mut prev_records = list_records.records.drain(..).collect::<Vec<_>>();
+                            let rt = list_records.get_next(&client)
+                                .map(|mut x| { 
+                                    prev_records.extend(x.records.drain(..));
+                                    x.records = prev_records;
+                                    x 
+                                });
+                            Ok(Loop::Continue(EitherFuture::B(rt)))
+                        } else {
+                            Ok(Loop::Break(list_records))
+                        }
+                    })
+                })
+
+            }
+
+            run(self.clone(), params)
+
+        }
+
     }
 }
 
@@ -240,12 +375,14 @@ mod ext {
     use roxmltree::{ExpandedName, Node};
 
     pub(crate) trait NodeExt<'a, 'd>: Sized {
-        fn first<I: Into<ExpandedName<'a>>>(&self, tag_name: I)
+        fn find_first_child<I: Into<ExpandedName<'a>>>(&self, tag_name: I)
             -> crate::err::Result<Node<'a, 'd>>;
+        fn find_first_child_parsed_text<P: std::str::FromStr, I: Into<ExpandedName<'a>>>(&'a self, tag_name: I)
+            -> crate::err::Result<P>;
     }
 
     impl<'a, 'd: 'a> NodeExt<'a, 'd> for Node<'a, 'd> {
-        fn first<I: Into<ExpandedName<'a>>>(
+        fn find_first_child<I: Into<ExpandedName<'a>>>(
             &self,
             tag_name: I
         ) -> crate::err::Result<Node<'a, 'd>>
@@ -254,7 +391,15 @@ mod ext {
             self.children()
                 .filter(|x| x.has_tag_name(tag_name))
                 .next()
-                .ok_or_else(|| crate::err::msg(format!("No tag named: {:?}", tag_name)))
+                .ok_or_else(|| crate::err::msg(format!("No such tag: {}", tag_name.name())))
+        }
+        fn find_first_child_parsed_text<P: std::str::FromStr, I: Into<ExpandedName<'a>>>(&'a self, tag_name: I)
+            -> crate::err::Result<P> {
+            let tag_name = tag_name.into();
+            self.find_first_child(tag_name)?
+                .text()
+                .and_then(|x| x.parse().ok() )
+                .ok_or_else(|| crate::err::msg(format!("No text for: {}", tag_name.name())))
         }
     }
 
